46b2685751d264a9e81f41bbadd95d8780073a75
Olivia Ten Den <olivia.ten-den@stu.ocsb.ca>
2025-04-24 17:06:17 +0000

    added template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia Ten Den
+// Created on: April 24, 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+function checkNumber() {
+  // input
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  // process
+  if (userNumber === randomNumber) {
+    //output
+    document.getElementById('result').innerHTML =
+      '<p>You guessed the right number!</p>'
+  }
+
+  // process
+  if (userNumber !== randomNumber) {
+    //output
+    document.getElementById('result').innerHTML =
+      '<p>You guessed the wrong number!' +
+      '<br>The correct number was: ' + randomNumber + '</p>'
+  }
+}
